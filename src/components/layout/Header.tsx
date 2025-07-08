90568a542545341fdc22b51dfd94a75a05cea59e
Hyeonjin <oboki2001@naver.com>
2025-07-08 19:26:26 +0900

    [style] Header 컴포넌트 UI 개선 및 스타일 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,19 +3,23 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex px-[20px] py-[10px] justify-between items-center bg-main border-b border-b-sub">
+    <header className="z-50 flex px-[30px] py-[10px] justify-between items-center bg-main">
       <Link
         href="/"
         className="font-ghanachoco text-[34px] text-white [text-shadow:2px_4px_4px_rgba(0,0,0,0.5)]"
       >
         Phraiz
       </Link>
-      <div className="flex gap-[30px] text-white text-[14px]">
+      <div className="flex gap-[10px] text-black text-[14px]">
         <Link href="/login" className="hover:font-nanum-extrabold">
-          로그인
+          <div className="bg-gradient-to-r from-white to-main backdrop-blur-md border-[0.2px] border-white/60 rounded-full py-[6px] px-[24px]">
+            로그인
+          </div>
         </Link>
         <Link href="/sign-up" className="hover:font-nanum-extrabold">
-          회원가입
+          <div className="bg-gradient-to-r from-white to-main backdrop-blur-md border-[0.2px] border-white/60 rounded-full py-[6px] px-[24px]">
+            회원가입
+          </div>
         </Link>
       </div>
     </header>
