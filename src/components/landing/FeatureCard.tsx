13e3ea6f9970621d564ab95f149648c99437ab18
Hyeonjin <oboki2001@naver.com>
2025-07-14 02:11:23 +0900

    [feat] 서비스 기능 소개용 FeatureCard 컴포넌트 구현 - 기능 제목, 설명, CTA 텍스트와 링크를 props로 받아 카드 구성 - hover 시 배경 그라데이션과 CTA 문구 등장 - 반응형 사이즈 및 hover 시 텍스트 투명도 처리

---
+++ src/components/landing/FeatureCard.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { ServiceFeature } from "@/constants/serviceLink";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+
+interface FeatureCardProps {
+  feature: ServiceFeature;
+  href: string;
+  ctaText: string;
+}
+
+const FeatureCard = ({ feature, href, ctaText }: FeatureCardProps) => {
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        "relative w-[290px] h-[180px] md:w-[350px] md:h-[240px] rounded-[16px] border border-[#a294f9] bg-gradient-to-r from-white to-main/30 flex flex-col items-center justify-center text-center overflow-hidden cursor-pointer group"
+      )}
+    >
+      <div
+        className="
+          absolute inset-0 z-20
+          bg-[linear-gradient(to_top,_#7752fe_0%,_#c1b2ff_30%,_transparent_60%)]
+          translate-y-full group-hover:translate-y-0
+          transition-transform duration-500 ease-out
+        "
+      />
+      {/* CTA 문구 */}
+      <span
+        className="
+          absolute bottom-[10px] z-20 flex items-center justify-center
+          font-nanum-bold md:text-[20px] text-[17px] text-white
+          opacity-0 group-hover:opacity-100
+          transition-opacity duration-300
+        "
+      >
+        {ctaText}
+        <Image
+          src="/icons/right_arrow.svg"
+          alt=">"
+          width={18}
+          height={10}
+          className="ml-[5px] w-[15px] h-[15px] md:w-[19px] md:h-[19px]"
+        />
+      </span>
+
+      {/* 기본 내용 (hover 시 희미해짐) */}
+      <div
+        data-fade
+        className="z-10 flex flex-col items-center justify-center text-center gap-[10px] px-6 w-full h-full
+        "
+      >
+        <h3
+          data-fade
+          className="font-nanum-extrabold md:text-[23px] text-[19px]"
+          dangerouslySetInnerHTML={{
+            __html: `${feature.title}`,
+          }}
+        ></h3>
+        <p
+          data-fade
+          className="md:text-[16px] text-[13px] group-hover:opacity-50"
+          dangerouslySetInnerHTML={{
+            __html: `${feature.description}`,
+          }}
+        ></p>
+      </div>
+    </Link>
+  );
+};
+
+export default FeatureCard;
