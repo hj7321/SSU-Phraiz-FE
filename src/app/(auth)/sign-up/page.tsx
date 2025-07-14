2b4976b7e62aa767eda2df495d189242683c26f6
Hyeonjin <oboki2001@naver.com>
2025-07-14 17:29:44 +0900

    [feat] 로그인, 회원가입 페이지 UI 기본 구조 구현 (InputWithLabel 사용)

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,19 @@
+import InputWithLabel from "@/components/ui/input/InputWithLabel";
+
 export default function SignUpPage() {
-  return <div>회원가입 페이지</div>;
+  return (
+    <section className="bg-gradient-to-b from-main to-main/20 h-[100vh] w-full flex flex-col gap-[20px] justify-center items-center">
+      <div className="flex flex-col gap-[25px] bg-white px-[50px] py-[60px] rounded-[12px] [filter:drop-shadow(0px_0px_10px_rgba(0,0,0,0.3))]">
+        <InputWithLabel
+          id="email"
+          type="email"
+          label="이메일"
+          placeholder="elitrekker@gmail.com"
+        />
+        <InputWithLabel id="id" type="text" label="아이디" />
+        <InputWithLabel id="pw" type="password" label="비밀번호" />
+        <InputWithLabel id="pw-check" type="password" label="비밀번호 확인" />
+      </div>
+    </section>
+  );
 }
