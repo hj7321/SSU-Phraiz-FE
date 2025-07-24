57c41a4362cb111e6b970c70de850866bdade607
Hyeonjin <oboki2001@naver.com>
2025-07-24 14:15:12 +0900

    [feat] 로그인 페이지에 입력 폼 마크업 개선 및 아이디 찾기 / 비밀번호 재설정 링크 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,7 @@ import useLoginForm from "@/hooks/useLoginForm";
 
 import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginPage() {
   const { id, setId, pw, setPw, handleLogin, handleSocialLogin, isLoggingIn } =
@@ -12,7 +13,7 @@ export default function LoginPage() {
 
   return (
     <section className="bg-gradient-to-b from-main to-main/20 h-[100vh] w-full flex flex-col gap-[20px] justify-center items-center">
-      <div className="flex flex-col gap-[25px] bg-white px-[50px] py-[60px] rounded-[12px] [filter:drop-shadow(0px_0px_10px_rgba(0,0,0,0.3))] mt-[-90px]">
+      <div className="flex flex-col gap-[25px] bg-white px-[50px] py-[60px] rounded-[12px] [filter:drop-shadow(0px_0px_10px_rgba(0,0,0,0.4))] mt-[-90px]">
         <div className="flex flex-col gap-[14px] items-center">
           <p className="text-[13px]">간편 로그인</p>
           <div className="flex gap-[20px]">
@@ -44,7 +45,7 @@ export default function LoginPage() {
         </div>
         <div className="flex items-center mt-[-5px] mb-[5px]">
           <div className="flex-grow border-t border-gray-300"></div>
-          <p className="flex-shrink mx-[8px] text-gray-500 text-sm">or</p>
+          <p className="flex-shrink mx-[8px] text-gray-500 text-[14px]">or</p>
           <div className="flex-grow border-t border-gray-300"></div>
         </div>
 
@@ -65,10 +66,11 @@ export default function LoginPage() {
             value={pw}
             onChange={(e) => setPw(e.target.value)}
           />
+
           <button
             type="submit"
             className={clsx(
-              "text-[14px] w-[250px]  text-white py-[10px] rounded-[4px] ",
+              "text-[14px] w-[255px]  text-white py-[10px] rounded-[4px] ",
               isLoggingIn ? "bg-main/40" : "bg-main/70 hover:bg-main"
             )}
             disabled={isLoggingIn}
@@ -76,6 +78,15 @@ export default function LoginPage() {
             {isLoggingIn ? "로그인 중..." : "로그인"}
           </button>
         </form>
+        <div className="text-[12px] flex justify-center gap-[6px]">
+          <Link href="/find-id" className="text-[#595959] hover:text-black">
+            아이디 찾기
+          </Link>
+          <div className="text-gray-500">│</div>
+          <Link href="/reset-pw" className="text-[#595959] hover:text-black">
+            비밀번호 재설정하기
+          </Link>
+        </div>
       </div>
     </section>
   );
