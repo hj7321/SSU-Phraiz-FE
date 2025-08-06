4531131ee6de3939088e6f2ab8b1333a849712e8
Hyeonjin <oboki2001@naver.com>
2025-08-06 15:43:37 +0900

    [refactor] 인용문 수정 UI에 반응형 Tailwind 스타일 적용

--- src/components/box/ChangeExistedCitationBox.tsx
+++ src/components/box/ChangeExistedCitationBox.tsx
@@ -40,27 +40,32 @@ const ChangeExistedCitationBox = () => {
   };
 
   return (
-    <div className="w-1/2 border-r p-[16px] flex flex-col gap-[20px]">
-      <div className="flex flex-col gap-[5px]">
-        <h1 className="text-[18px]">변환할 인용문을 입력하세요</h1>
+    <div className="p-[16px] flex flex-col gap-[10px] md:gap-[15px] w-full">
+      <div className="flex flex-col gap-[2px] md:gap-[5px]">
+        <h1 className="text-[14px] sm:text-[16px] md:text-[18px]">
+          변환할 인용문을 입력하세요
+        </h1>
         <Input
           type="text"
           value={citationValue}
           onChange={(e) => setCitationValue(e.target.value)}
+          className="text-[12px] sm:text-[14px] md:text-[16px] h-[26px] sm:h-[30px] md:h-[36px] px-[6px] sm:px-[8px] md:px-[12px]"
         />
       </div>
-      <div className="flex flex-col gap-[5px]">
-        <h1 className="text-[18px]">변환할 인용 형식을 선택하세요</h1>
+      <div className="flex flex-col gap-[2px] md:gap-[5px]">
+        <h1 className="text-[14px] sm:text-[16px] md:text-[18px]">
+          변환할 인용 형식을 선택하세요
+        </h1>
         <SelectScrollable
           selectedForm={selectedForm}
           setSelectedForm={setSelectedForm}
         />
       </div>
-      <div className="mt-[20px]">
+      <div className="mt-[5px]">
         <button
           onClick={handleChangeCitation}
           disabled
-          className="bg-main/40 text-white py-[10px] px-[16px] rounded-[6px]"
+          className="bg-main/40 text-white py-[6px] px-[10px] md:py-[10px] md:px-[16px] rounded-[6px] text-[10px] sm:text-[13px] md:text-[16px]"
         >
           인용문 변환하기
         </button>
