0635a23d7174ea7cd0f6897196d94e631ed0851d
Hyeonjin <oboki2001@naver.com>
2025-08-06 15:50:08 +0900

    [style] Footer 반응형 텍스트 크기 조정 및 마크업 개선

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-end text-[12px] bg-main/20 h-[300px] items-center pb-[30px]">
+    <footer className="flex flex-col justify-end text-[9px] md:text-[12px] bg-main/20 h-[300px] items-center pb-[30px]">
       <p>2025 IT Project | 김현진 김희서 안선아 조은빈 | Soongsil University</p>
       <p>Copyright © 2025 Codiva. All rights reserved.</p>
     </footer>
