411418fbef719c77cff493af90052eaeb4af8e3a
Hyeonjin <oboki2001@naver.com>
2025-08-06 15:49:34 +0900

    [feature] LandingSection에 pretitle 지원 및 텍스트 구조 개선

--- src/components/landing/LandingSection.tsx
+++ src/components/landing/LandingSection.tsx
@@ -49,10 +49,17 @@ const LandingSection = ({ data, idx }: LandingSectionProps) => {
     >
       <h1
         data-fade
-        className="text-[30px] sm:text-[36px] md:text-[46px] lg:text-[50px]  font-nanum-extrabold [filter:drop-shadow(4px_4px_10px_rgba(0,0,0,0.2))] bg-gradient-to-r from-[#7752fe] via-[#828ffa] to-[#7752fe] bg-clip-text text-transparent mb-[-10px]"
+        className="hidden md:block text-[22px] xs:text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px]  font-nanum-extrabold [filter:drop-shadow(4px_4px_10px_rgba(0,0,0,0.2))] bg-gradient-to-r from-[#7752fe] via-[#828ffa] to-[#7752fe] bg-clip-text text-transparent mb-[-10px]"
       >
         {data.title}
       </h1>
+      <h1
+        data-fade
+        className="block md:hidden text-[22px] xs:text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px]  font-nanum-extrabold [filter:drop-shadow(4px_4px_10px_rgba(0,0,0,0.2))] bg-gradient-to-r from-[#7752fe] via-[#828ffa] to-[#7752fe] bg-clip-text text-transparent mb-[-10px] text-center"
+        dangerouslySetInnerHTML={{
+          __html: `${data.preTitle ? data.preTitle : data.title}`,
+        }}
+      ></h1>
 
       <div className="relative w-[240px] h-[160px] sm:w-[270px] sm:h-[180px] md:w-[300px] md:h-[200px] lg:w-[360px] lg:h-[240px]">
         <Image
