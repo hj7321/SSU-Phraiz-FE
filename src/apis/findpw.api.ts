23e23d93272dcf64f858b40cc895b53fb1866f3a
Hyeonjin <oboki2001@naver.com>
2025-08-08 10:23:35 +0900

    [refactor] sendResetPwLink·resetPassword 함수 타입 지정 및 반환 데이터 구조 개선

--- src/apis/findpw.api.ts
+++ src/apis/findpw.api.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { api } from "./api";
 import { ResetPasswordType } from "@/types/auth.type";
+import { SuccessResponseData } from "@/types/common.type";
 
 // 비밀번호 재설정 링크 전송
-export const sendResetPwLink = async (email: string) => {
+export const sendResetPwLink = async (
+  email: string
+): Promise<SuccessResponseData> => {
   const path = "/members/findPwd";
 
   try {
-    const response = await api.post(path, { email: email });
-    return response;
+    const response = await api.post<SuccessResponseData>(path, {
+      email: email,
+    });
+    return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       if (error.response?.data.code) {
@@ -26,11 +31,17 @@ export const sendResetPwLink = async (email: string) => {
 };
 
 // 비밀번호 재설정
-export const resetPassword = async ({ token, newPwd }: ResetPasswordType) => {
+export const resetPassword = async ({
+  token,
+  newPwd,
+}: ResetPasswordType): Promise<SuccessResponseData> => {
   const path = "/members/resetPwd";
 
   try {
-    const response = await api.post(path, { token: token, newPwd: newPwd });
+    const response = await api.post<SuccessResponseData>(path, {
+      token: token,
+      newPwd: newPwd,
+    });
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
