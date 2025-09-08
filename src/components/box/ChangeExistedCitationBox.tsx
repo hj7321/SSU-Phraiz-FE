e87f92033097613c1e21f0b2e7995d56b5be64c1
Hyeonjin <oboki2001@naver.com>
2025-09-08 13:39:40 +0900

    [feat] 기존 인용문 변경 기능 구현 및 UI 개선

--- src/components/box/ChangeExistedCitationBox.tsx
+++ src/components/box/ChangeExistedCitationBox.tsx
@@ -5,30 +5,35 @@ import { Input } from "../ui/input/input";
 import SelectScrollable from "../ui/select/SelectScrollable";
 import { useAuthStore } from "@/stores/auth.store";
 import { useRouter } from "next/navigation";
+import { generateCitation } from "@/utils/citation";
+import clsx from "clsx";
+import { useCitationStore } from "@/stores/citation.store";
+import { useMutation } from "@tanstack/react-query";
+import { sendCitation } from "@/apis/citation.api";
 
 const ChangeExistedCitationBox = () => {
   const [citationValue, setCitationValue] = useState<string>("");
   const [selectedForm, setSelectedForm] = useState<string | undefined>(
     undefined
   );
+  const [citationResult, setCitationResult] = useState<string>("");
 
   const isLogin = useAuthStore((s) => s.isLogin);
+  const setCitation = useCitationStore((s) => s.setCitation);
   const router = useRouter();
 
-  // 인용문 변환 뮤테이션
-  // const {
-  //   mutate
-  // } = useMutation({
-  //   mutationKey: ["api 함수명", citationValue, selectedForm],
-  //   mutationFn: api 함수,
-  //   onSuccess: (response) => {
-  //     console.log("✅ 인용문 변환 성공", response);
-  //   },
-  //   onError: (err) => {
-  //     console.error("❌ 인용문 변환 실패: ", err.message);
-  //     alert(err.message);
-  //   },
-  // });
+  // 인용문 전송 뮤테이션
+  const { mutate: sendCitationMutate } = useMutation({
+    mutationKey: ["sendCitation", citationResult],
+    mutationFn: sendCitation,
+    onSuccess: (data) => {
+      console.log("✅ 인용문 전달 성공", data);
+    },
+    onError: (err) => {
+      console.error("❌ 인용문 전달 실패: ", err.message);
+      alert(err.message);
+    },
+  });
 
   // 인용문 변환 핸들러
   const handleChangeCitation = () => {
@@ -37,6 +42,26 @@ const ChangeExistedCitationBox = () => {
       router.push("/login");
       return;
     }
+
+    if (!citationValue || !citationValue.trim()) {
+      alert("인용문을 입력해주세요.");
+      return;
+    }
+    if (!selectedForm) {
+      alert("인용 형식을 선택해주세요.");
+      return;
+    }
+
+    const result = generateCitation(citationValue, selectedForm);
+    if (result !== "") {
+      setCitationResult(result);
+      setCitation(result);
+    }
+    // sendCitationMutate({
+    //     // citeId: 없음...,
+    //     citation: result,
+    //     style: selectedForm!,
+    //   });
   };
 
   return (
@@ -64,8 +89,13 @@ const ChangeExistedCitationBox = () => {
       <div className="mt-[5px]">
         <button
           onClick={handleChangeCitation}
-          disabled
-          className="bg-main/40 text-white py-[6px] px-[10px] md:py-[10px] md:px-[16px] rounded-[6px] text-[10px] sm:text-[13px] md:text-[16px]"
+          disabled={!citationValue || !selectedForm}
+          className={clsx(
+            "text-white py-[6px] px-[10px] md:py-[10px] md:px-[16px] rounded-[6px] text-[10px] sm:text-[13px] md:text-[16px]",
+            !citationValue || !selectedForm
+              ? "bg-main/40"
+              : "bg-main/70 hover:bg-main"
+          )}
         >
           인용문 변환하기
         </button>
