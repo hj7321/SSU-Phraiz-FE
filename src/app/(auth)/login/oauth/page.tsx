1196b5866109b254e4f518211362e4a8736028bc
Hyeonjin <oboki2001@naver.com>
2025-09-26 15:04:14 +0900

    [feat] SEO 개선을 위한 metadata 추가

--- src/app/(auth)/login/oauth/page.tsx
+++ src/app/(auth)/login/oauth/page.tsx
@@ -1,5 +1,17 @@
 import { Suspense } from "react";
-import OAuthRedirectPageContent from "./OAuthRedirectPageContent";
+import { Metadata } from "next";
+import dynamic from "next/dynamic";
+
+const OAuthRedirectPageContent = dynamic(
+  () => import("./OAuthRedirectPageContent"),
+  { ssr: false }
+);
+
+export const metadata: Metadata = {
+  title: "소셜 로그인",
+  robots: { index: false, follow: false },
+  alternates: { canonical: "/login/oauth" },
+};
 
 export default function OAuthRedirectPage() {
   return (
