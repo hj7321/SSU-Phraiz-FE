6efbf3ae6f46de34d1301832efdd1a341d92092e
Hyeonjin <oboki2001@naver.com>
2025-09-26 15:14:14 +0900

    [refactor] 로그인 페이지 구조 리팩토링 및 메타데이터 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,83 +1,36 @@
-"use client";
-
-import InputWithLabel from "@/components/ui/input/InputWithLabel";
-import useLoginForm from "@/hooks/useLoginForm";
-
-import clsx from "clsx";
-import Image from "next/image";
+import SocialButtons from "@/components/button/SocialButtons";
+import LoginForm from "@/components/form/LoginForm";
+import { Metadata } from "next";
 import Link from "next/link";
 
-export default function LoginPage() {
-  const { id, setId, pw, setPw, handleLogin, handleSocialLogin, isLoggingIn } =
-    useLoginForm();
+export const metadata: Metadata = {
+  title: "로그인",
+  robots: { index: false, follow: false },
+  alternates: { canonical: "/login" },
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://ssu-phraiz-fe.vercel.app/login",
+    title: "Phraiz",
+    description: "로그인",
+    images: ["/opengraph/login.jpg"],
+  },
+};
 
+export default function LoginPage() {
   return (
     <section className="bg-gradient-to-b from-main to-main/20 h-[100vh] w-full flex flex-col gap-[20px] justify-center items-center">
       <div className="flex flex-col gap-[25px] bg-white px-[50px] py-[60px] rounded-[12px] [filter:drop-shadow(0px_0px_10px_rgba(0,0,0,0.4))] mt-[-90px]">
         <div className="flex flex-col gap-[14px] items-center">
           <p className="text-[13px]">간편 로그인</p>
-          <div className="flex gap-[20px]">
-            <button onClick={() => handleSocialLogin("google")}>
-              <Image
-                src="/images/button_google.svg"
-                alt="google"
-                width={55}
-                height={55}
-              />
-            </button>
-            <button onClick={() => handleSocialLogin("naver")}>
-              <Image
-                src="/images/button_naver.svg"
-                alt="naver"
-                width={55}
-                height={55}
-              />
-            </button>
-            <button onClick={() => handleSocialLogin("kakao")}>
-              <Image
-                src="/images/button_kakao.svg"
-                alt="kakao"
-                width={55}
-                height={55}
-              />
-            </button>
-          </div>
+          <SocialButtons />
         </div>
         <div className="flex items-center mt-[-5px] mb-[5px]">
           <div className="flex-grow border-t border-gray-300"></div>
           <p className="flex-shrink mx-[8px] text-gray-500 text-[14px]">or</p>
           <div className="flex-grow border-t border-gray-300"></div>
         </div>
-
-        <form onSubmit={handleLogin} className="flex flex-col gap-[25px]">
-          <InputWithLabel
-            id="id"
-            name="id"
-            type="text"
-            label="아이디"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-          <InputWithLabel
-            id="pw"
-            name="pw"
-            type="password"
-            label="비밀번호"
-            value={pw}
-            onChange={(e) => setPw(e.target.value)}
-          />
-
-          <button
-            type="submit"
-            className={clsx(
-              "text-[14px] w-[255px] text-white py-[10px] rounded-[4px] ",
-              isLoggingIn ? "bg-main/40" : "bg-main/70 hover:bg-main"
-            )}
-            disabled={isLoggingIn}
-          >
-            {isLoggingIn ? "로그인 중..." : "로그인"}
-          </button>
-        </form>
+        <LoginForm />
         <div className="text-[12px] flex justify-center gap-[6px]">
           <Link href="/find-id" className="text-[#595959] hover:text-black">
             아이디 찾기
