9e04678f604cdf3ccaa4bd80c4387c1f937d5e4f
Hyeonjin <oboki2001@naver.com>
2025-10-30 19:14:24 +0900

    배포 환경에서의 에러 해결

--- src/utils/citation.ts
+++ src/utils/citation.ts
@@ -4,7 +4,7 @@ import { CSL } from "@/types/citation.type";
 
 export const generateCitation = (cslItem: CSL | string, style: string) => {
   const citation = new Cite(cslItem).format("bibliography", {
-    template: style,
+    template: style.toLowerCase(),
     lang: "ko-KR",
     format: "text",
   });
